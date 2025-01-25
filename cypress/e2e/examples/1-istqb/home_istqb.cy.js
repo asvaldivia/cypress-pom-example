8a69bb154160e87921c0537d7239aea7bf59cc57
asvaldivia <asvaldivia12@gmail.com>
2025-01-24 23:54:18 -0400

    istqb tests, no pom

--- cypress/e2e/examples/1-istqb/home_istqb.cy.js
+++ cypress/e2e/examples/1-istqb/home_istqb.cy.js
@@ -10,7 +10,7 @@ describe('Home of ISTQB', () => {
         cy.title().should('include', 'International Software Testing Qualifications Board');
     })
 
-    it('should find certifications', () => {
+    it.skip('should find certifications', () => {
         let categoriesTextArray;
         // clicking on certifications
         cy.get('ul.header-nav.header-nav-main.nav.nav-left.nav-line-bottom.nav-size-medium.nav-spacing-xlarge > li:nth-child(2)', {timeout: 10000}).click();
@@ -25,34 +25,50 @@ describe('Home of ISTQB', () => {
             // should be at least 1 courses available for each category
             categoriesTextArray.forEach(category => {
                 // use the cy.get(#id).contains('text') to find the category
-                cy.get('div.text h2').each((element) => {
-                    cy.task('logMessage', 'This is the element text: '+ element.text());
+                cy.get('div.text h2').each(($element) => {
+                    const elementText = $element.text().trim();
+                    cy.task('logMessage', 'This is the element text: '+ elementText);
                     cy.task('logMessage', 'This is the category: '+ category);
                     
-                    if(element.text() === category){
-                        const textOfElement = element.text();
-                        cy.task('logMessage', 'The text of the element '+textOfElement+' is matching with the category '+category+' !!!');
-                        
+                    if(elementText === category){
                         // find the parent of the category
-                        const parent = element.parents('div').siblings();
-                        cy.task('logMessage', 'Parent tag name: ' + parent.prop('tagName'));
-                        cy.task('logMessage', 'Parent classes: ' + parent.attr('class'));
-                        cy.task('logMessage', 'Parent HTML content: ' + parent.html());
-                        // find the sibling of the parent
-                        //const sibling = parent.siblings();
-                        // find the courses
-                        //const courses = sibling.find('div.text h3');
-                        // log the number of courses
-                        //cy.task('logMessage', 'Number of courses found for category {{category}}: ', courses.length);
-                        //expect(courses.length).to.be.greaterThan(1);
+                        cy.wrap($element)
+                        .parentsUntil('div.section-content.relative')
+                        .find('div.text h3')
+                        .then(($certifications) => {
+                            // assert more than 1 certification by category
+                            expect($certifications.length).to.be.greaterThan(0);
+                        });
                     }
                 })
-        })
+            })
         })
     })
 
-    it.skip('should find training providers', () => {
+    function acceptCookiesIfExist(){
+        try{
+            cy.get('.primary > span').then(($button) => {
+                if($button.length > 0){
+                    cy.wrap($button).click();
+                }
+            })
+        } catch(error) {
+            cy.task('logMessage', 'No cookies found');
+        }
+            
+    }
+
+    it('should find training providers', () => {
         // Check more than 2 training providers
+        cy.get('#menu-item-35 > a', {timeout: 10000}).click();
+        cy.get('div.text p > a[href="/training-providers?directory_type=training-providers"]').click();
+        cy.get('#desktop-dropdowns > div.w-full.flex.items-center.mb-6 > button:nth-child(1)').click();
+        acceptCookiesIfExist();
+        cy.get('div#desktop-results', {timeout: 20000}).then(($providers) => {
+            cy.wrap($providers)
+            .find('tr')
+            .should('have.length.greaterThan', 2);
+        });
 
     })
     
